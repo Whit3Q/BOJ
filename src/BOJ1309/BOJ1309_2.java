2b90d2489e774a22be0aeab9031d062ccecd7a8f
whit3Q <gw1221@naver.com>
2022-08-04 01:02:31 +0900

    BOJ1309_2

--- src/BOJ1309/BOJ1309_2.java
+++ src/BOJ1309/BOJ1309_2.java
@@ -6,29 +6,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class BOJ1309 {
+public class BOJ1309_2 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static int N;
     static int MOD = 9901;
+    static int[][] field;
 
-
-    public static int solve() {
-        int num2 = 0;
-        int num = 1;
-        int now = 3;
-        for (int i = 2; i < N+1; i++) {
-            int tmp = now;
-            now = (((now * 2) % MOD) + (num % MOD)) % MOD;
-            num = tmp;
+    public static int solve(){
+        for(int i = 1; i < N+1; i++) {
+            field[0][i] = (field[0][i-1] + field[1][i-1] + field[2][i-1])%MOD;
+            field[1][i] = (field[0][i-1] + field[2][i-1])%MOD;
+            field[2][i] = (field[0][i-1] + field[1][i-1])%MOD;
         }
 
-        return now;
+        return field[0][N];
     }
 
     public static void main(String[] args) throws IOException {
 
         N = Integer.parseInt(br.readLine());
+        field = new int[3][N+1];
+        field[0][0] = 1;
+        field[1][0] = 1;
+        field[2][0] = 1;
 
         bw.write(solve()+"");
         bw.flush();
