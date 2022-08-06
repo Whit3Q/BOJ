7b50b7cc48d9b876a362fa5abe5d78f0c3c9056e
whit3Q <gw1221@naver.com>
2022-08-06 21:16:28 +0900

    BOJ2293

--- src/BOJ2293/BOJ2293.java
+++ src/BOJ2293/BOJ2293.java
@@ -16,24 +16,6 @@ public class BOJ2293 {
     static int N;
     static int K;
 
-        public int solution(int[] numbers, int target) {
-
-            return solve(numbers, target, 1, numbers[0]) + solve(numbers, target, 1, -numbers[0]);
-        }
-        private int solve(int[] numbers, int target, int index, int sum) {
-            if(numbers.length == index ) {
-                if(target == sum) {
-                    return 1;
-                }
-                return 0;
-            }
-
-            int result = solve(numbers, target, index + 1, sum + numbers[index])
-                    + solve(numbers, target, index + 1, sum - numbers[index]);
-
-            return result;
-        }
-
     public static void main(String[] args) throws IOException {
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
@@ -42,7 +24,6 @@ public class BOJ2293 {
         for(int i = 0; i < N; i++) {
             list[i] = Integer.parseInt(br.readLine());
         }
-
 
         dp[0] = 1;
         for (int i = 0; i < N; i++) {
