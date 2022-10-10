02cde8038420a81fc02e28de1fa175f6441e9a87
whit3Q <gw1221@naver.com>
2022-10-11 02:46:10 +0900

    no message

--- src/BOJ17135/Main.java
+++ src/BOJ17135/Main.java
@@ -5,10 +5,10 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
-import java.util.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
